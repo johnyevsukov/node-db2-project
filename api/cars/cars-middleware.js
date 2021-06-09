5440b46ba710d3f56cf9e2ab9701eafd2fd8adfb
John Yevsukov <github.com@john.engineering>
2021-06-08 23:34:53 -0400

    mvp

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,64 @@
+const Car = require('./cars-model')
+const vinValidator = require('vin-validator')
+
+
 const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
+  Car.getById(req.params.id)
+  .then(car => {
+    if(!car) {
+      res.status(404).json({ message: `car with id ${req.params.id} is not found` })
+    }
+    else {
+      req.car = car
+      next()
+    }
+  })
 }
 
 const checkCarPayload = (req, res, next) => {
-  // DO YOUR MAGIC
+  const { vin, make, model, mileage } = req.body
+
+  if(vin == undefined) {
+    res.status(400).json({ message: 'vin is missing' })
+  }
+  else if(make == undefined) {
+    res.status(400).json({ message: 'make is missing' })
+  }
+  else if(model == undefined) {
+    res.status(400).json({ message: 'model is missing' })
+  }
+  else if(mileage == undefined) {
+    res.status(400).json({ message: 'mileage is missing' })
+  }
+  else {
+    next()
+  }
 }
 
 const checkVinNumberValid = (req, res, next) => {
-  // DO YOUR MAGIC
+  if(vinValidator.validate(req.body.vin)){
+    next()
+  }
+  else {
+    res.status(400).json({ message: `vin ${req.body.vin} is invalid` })
+  }
 }
 
 const checkVinNumberUnique = (req, res, next) => {
-  // DO YOUR MAGIC
+  Car.getByVin(req.body.vin)
+  .then(car => {
+    if(car) {
+      res.status(400).json({ message: `vin ${req.body.vin} already exists` })
+    }
+    else {
+      next()
+    }
+  })
+}
+
+module.exports = {
+  checkCarId,
+  checkVinNumberValid,
+  checkCarPayload,
+  checkVinNumberUnique
 }
